11ddebcfce7e4ff353e9eb204de855debc93c973
shubham-rawal <shubhamrawal2001@gmail.com>
2022-09-01 21:55:19 +0530

    interactive map of desired latitude and longitude

--- BucketList/ContentView.swift
+++ BucketList/ContentView.swift
@@ -5,12 +5,14 @@
 //  Created by Shubham Rawal on 31/08/22.
 //
 
+import MapKit
 import SwiftUI
 
 struct ContentView: View {
+    @State private var mapRegion = MKCoordinateRegion(center: CLLocationCoordinate2D(latitude: 51.5, longitude: -0.12), span: MKCoordinateSpan(latitudeDelta: 0.2, longitudeDelta: 0.2))
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        Map(coordinateRegion: $mapRegion)
     }
 }
 
